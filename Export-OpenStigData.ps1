747c54a9deac78fe74339120992934f328a67522
heuricane <38864054+heuricane@users.noreply.github.com>
2018-04-30 15:37:53 -0400

    Works in progress
    Might work to combine them when I know what about my job annoys me the most that I must automate.

---
+++ Export-OpenStigData.ps1
@@ -0,0 +1,58 @@
+﻿<#
+.SYNOPSIS
+    Saves a CSV containing information on all Open stigs
+.DESCRIPTION
+    Saves a CSV containing information on all Open stigs
+.PARAMETER CKLDirectory
+    Full path to a directory containing your checklists
+  
+.EXAMPLE
+    .\Export-OpenStigData.ps1 -CKLDirectory 'C:\dwn\' -SavePath 'C:\tmp\OpenChecks.csv'
+#>
+Param([Parameter(Mandatory=$true)]$CKLDirectory,[Parameter(Mandatory=$true)]$SavePath)
+#Check if module imported
+if ((Get-Module | Where-Object -FilterScript {$_.Name -eq "StigSupport"}).Count -le 0)
+{
+    #End if not
+    Write-Error "Please import StigSupport.psm1 before running this script"
+    return
+}
+
+#List all CKL Files
+$CKLs = Get-ChildItem -Path $CKLDirectory -Filter "*.ckl"
+
+#Initialize Results
+$STIGData = @()
+
+Foreach ($CKL in $CKLs)
+{
+    #Load this CKL
+    $CKLData = Import-StigCKL -Path $CKL.FullName
+    $HostData = Get-CKLHostData -CKLData $CKLData
+    #Grab data on all stigs.
+    #Format of @{Status,Finding,Comments,VulnID,Comments}
+    $Stigs = Get-VulnCheckResult -XMLData $CKLData
+    $Asset = $HostData.HostName
+    foreach ($Stig in $Stigs)
+    {
+        #Requested STIG, Vuln ID, Rule Title/Name, Check Content, Fix Text, Comments
+        if ($Stig.Status -notmatch "NotAFinding") {
+            Write-Host "Need $($Stig.VulnID)"
+            if (($STIGData | Where-Object -FilterScript {$_.VulnID -eq $Stig.VulnID})) {
+                ($STIGData | Where-Object -FilterScript {$_.VulnID -eq $Stig.VulnID}).Count++;
+            } else {
+                $ToAdd = New-Object -TypeName PSObject -Property @{VulnID=$Stig.VulnID;RuleTitle="";CheckContent="";FixText="";Comments="";Asset=""}
+                $ToAdd.RuleTitle = Get-VulnInfoAttribute -XMLData $CKLData -VulnID $Stig.VulnID -Attribute "Rule_Title"
+                $ToAdd.CheckContent = Get-VulnInfoAttribute -XMLData $CKLData -VulnID $Stig.VulnID -Attribute "Check_Content"
+                $ToAdd.FixText = Get-VulnInfoAttribute -XMLData $CKLData -VulnID $Stig.VulnID -Attribute "Fix_Text"
+                $ToAdd.Comments = Get-VulnInfoAttribute -XMLData $CKLData -VulnID $Stig.VulnID -Attribute "Comment"
+                $ToAdd.Asset = $Asset
+                $STIGData += $ToAdd
+            }
+        } else {
+            Write-Host "Skip $($Stig.VulnID)"
+        }
+    }
+}
+
+$STIGData | Export-Csv -Path $SavePath -NoTypeInformation
---
+++ Set-STIGComments.ps1
@@ -0,0 +1,42 @@
+﻿$user = "Jay Berkovitz WSMIS ISSO"
+$initpath = "c:\dwn\"
+
+# This function creates the dialog box to choose the checklist file
+
+Function Get-FileName($initialDirectory)
+{   
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.ShowHelp = $true
+    $OpenFileDialog.filter = "All files (*.ckl)| *.ckl"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+} #end function Get-FileName
+
+#Sets the $path variable to the file you chose in the dialog box
+$path = Get-FileName -initialDirectory $initpath
+
+#Loads the contents as XML
+$xml = [xml](Get-Content $path)
+
+
+#sets the $date variable to the current date and formats it as dd/mm/YYYY and formats the savedate to ISO yyyyMMdd
+$date = Get-Date -format d
+$savedate = (Get-Date).tostring("yyyyMMdd")
+
+<#
+For each $Attr (node) at the VULN level of the tree, 
+check the STATUS node for a match to "NotAFinding" 
+and then set the COMMENTS node to "Reviewed by Username on dd/mm/YYYY"
+#>
+ForEach ($Attr in $xml.CHECKLIST.STIGS.iSTIG.VULN) {
+    If ($Attr.STATUS -match "NotAFinding") {
+        $Attr.COMMENTS = "Reviewed by $user on $date"
+    }
+}
+
+#Save the now modified xml back to the file you initially loaded.
+$destination = Split-Path -Path $path -Parent
+$filename = [io.path]::GetFileNameWithoutExtension("$path")
+$xml.Save($destination + "\" + $filename + "_$savedate.ckl")
